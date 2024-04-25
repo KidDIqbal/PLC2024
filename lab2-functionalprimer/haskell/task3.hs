65ed4fb965c3e9b944e7cda2729d7d84efff0f19
Kid Iqbal <220119832@aston.ac.uk>
2024-04-25 02:31:57 +0000

    hey

--- lab2-functionalprimer/haskell/task3.hs
+++ lab2-functionalprimer/haskell/task3.hs
@@ -1,7 +1,12 @@
-sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
+sgn :: (Ord a, Num a) => a -> Int
+sgn x  | x < 0 = -1 
+       | x == 0 = 0 
+       | otherwise = 1
 
 main = do
     putStrLn "Please enter a number"
     input <- getLine 
-    let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
+    let x = (read input :: Int) -- read converts any string into any other value you need.
+    putStrLn( show (sgn (x)) ) -- show converts any value into a string.
+
+    -- using these two functions is a form of input validation, in that it checks that user input can be an integer, before using it as a string again.
